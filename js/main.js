4140e590ea27c2875ae895aa6b61bc0ebb6db0c3
VidyChan <renukaghate@ymail.com>
2014-04-19 00:10:26 +0530

    A bit of modification and the menu is fixed for mobile view, need to edit css and all yet. :)

--- js/main.js
+++ js/main.js
@@ -8,10 +8,14 @@ $(document).ready(function () {
         $('.home').css('height', $act).css('width', $wid);
         $('.home .bg').css('height', $act).css('width', $wid);
 
-        
+
         if ($(window).height() < ($('nav').offset().top + 50)) {
             $('.home').css('height', $('nav').offset().top + 200);
             $('.home .bg').css('height', $('nav').offset().top + 200);
+        }
+        if ($(window).height() < 424) {
+            $('.home').css('min-height', 500);
+            $('.home .bg').css('min-height', 500);
         }
     }
 
@@ -20,6 +24,17 @@ $(document).ready(function () {
     $(window).resize(function () {
         //var $act = $(window).height();
         setStuff($act)
+    });
+
+    $('.namee span').mouseover(function (e) {
+        e.preventDefault();
+        var tooltip = $(this).attr('data-title');
+        console.log(tooltip);
+        $(this).css('position', 'relative');
+        $(this).append('<span class="tooltip">' + tooltip + '</span>');
+    });
+    $('.namee span').mouseout(function () {
+        $(this).find('span.tooltip').hide().remove();
     });
 
 });
